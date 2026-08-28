75f1b3550bdf10887c2223c2c3bfe625569b23d2
Dima <rdmitry.f1@gmail.com>
2026-08-28 14:51:48 +0500

    feat(customer-service): add validation messages to properties

--- services/customer-service/src/main/java/io/delivhub/customerservice/dtos/request/city/CreateCityRequest.java
+++ services/customer-service/src/main/java/io/delivhub/customerservice/dtos/request/city/CreateCityRequest.java
@@ -4,8 +4,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record CreateCityRequest(
-        @NotBlank(message = "Caty name cannot be empty")
-        @Size(max = 255, message = "City name must be less than 255 characters")
+        @NotBlank(message = "{validation.city.name.notBlank}")
+        @Size(max = 255, message = "{validation.city.name.size}")
         String name
 ) {
 }
