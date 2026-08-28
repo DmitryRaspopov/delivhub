2f69f4d9b115293667d559ae774247f21725a87d
Dima <rdmitry.f1@gmail.com>
2026-08-28 15:01:18 +0500

    feat(customer-service): add Location header to City save response

--- services/customer-service/src/main/java/io/delivhub/customerservice/controllers/CityController.java
+++ services/customer-service/src/main/java/io/delivhub/customerservice/controllers/CityController.java
@@ -15,6 +15,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/cities")
@@ -49,8 +52,16 @@ public class CityController {
     public ResponseEntity<CityResponse> save(
             @Valid @RequestBody CreateCityRequest request
     ) {
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(cityService.save(request));
+
+        CityResponse savedCity = cityService.save(request);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedCity.id())
+                .toUri();
+
+        return ResponseEntity.created(location).body(savedCity);
     }
 
 
